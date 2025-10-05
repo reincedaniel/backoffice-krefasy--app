a76101ce04f264e92e349584b56fd2d58fbb83da
Lourenço Carlos <reincedaniel@gmail.com>
2025-10-05 17:42:46 +0100

    refactor: base build ups

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="dark:text-white-dark text-center ltr:sm:text-left rtl:sm:text-right p-6 pt-0 mt-auto">
-        © {{ new Date().getFullYear() }}. Vristo All rights reserved.
+        © {{ new Date().getFullYear() }}. Krefasy - Mayacode & Enzadaz.
     </div>
 </template>
